87e375cbac8576c233748286e8c28fbe0777476a
torres <estexerpruebas@gmail.com>
2019-06-19 09:50:55 +0200

    primer commit de Ejercicios de Java

--- 801_Git_Ejercicios/src/com/basico/Operadores.java
+++ 801_Git_Ejercicios/src/com/basico/Operadores.java
@@ -4,8 +4,8 @@ public class Operadores {
 
 	public static void main(String[] args) {
 	
-		int n1=15;
-		int n2=16;
+		int n1=100;
+		int n2=200;
 
 		System.out.println("La suma es " + (n1+n2));
 		System.out.println("La resta es " + (n1-n2));
